26ef5d9e8cd20f189c37cf46a47ddada5f960805
sanchitsetia <sanchitsetia1205@gmail.com>
2025-03-09 20:15:08 +0530

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl">Hello</h1>
+      <Button variant={"destructive"}>hey</Button>
+    </>
+  );
+}
+
+export default App;
